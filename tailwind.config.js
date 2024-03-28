82deaddb4df2fc9a287d1e4cb3c95cce384bad25
mohamedsadokmandhouj <162150588+mohamedsadokmandhouj@users.noreply.github.com>
2024-03-28 09:30:11 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./home.html","./projects.html","./contact.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
